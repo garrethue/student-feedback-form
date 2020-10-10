a787c94fd402922755a2e80a44301ec258d9b01f
Garret Larson <garret.hs.larson@gmail.com>
2020-10-10 09:49:15 -0500

    Added chakra UI.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+//import axios from 'axios';
+import "./App.css";
+import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
+import customTheme from "../theme";
+
+// function App({ children }) {
+//   return (
+//     <ThemeProvider theme={customTheme}>
+//       <CSSReset />
+//       {children}
+//     </ThemeProvider>
+//   );
+// }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <ThemeProvider theme={customTheme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Feedback!</h1>
+              <h4>
+                <i>Don't forget it!</i>
+              </h4>
+            </header>
+            <br />
+          </div>
+        </ColorModeProvider>
+      </ThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { logger } from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feelingReducer = (state = 0, action) => {
+  return state;
+};
+
+const understandingReducer = (state = 0, action) => {
+  return state;
+};
+
+const supportedReducer = (state = 0, action) => {
+  return state;
+};
+
+const commentsReducer = (state = "", action) => {
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    feeling: feelingReducer,
+    understanding: understandingReducer,
+    supported: supportedReducer,
+    comments: commentsReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
