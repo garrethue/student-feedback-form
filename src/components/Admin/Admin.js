e3d30d69a8911c42bf6429af88c034adae77d489
Garret Larson <garret.hs.larson@gmail.com>
2020-10-12 09:45:41 -0500

    Changed table structure so that it is Chakra-UI compliant.

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import "./Admin.css";
+import React, { useState, useEffect, Fragment } from "react";
 import { Grid, Box, Text, Button } from "@chakra-ui/core";
 import axios from "axios";
 
@@ -53,23 +52,130 @@ export default function Admin() {
           </Text>
         </Box>
         <Box marginBottom={3} rounded={3} w="100%" bg="blue.500">
-          <table>
-            <tr>
-              <th>Feeling</th>
-              <th>Comprehension</th>
-              <th>Support</th>
-              <th>Comments</th>
-              <th>Delete</th>
-              <th>Flag</th>
-            </tr>
+          <Grid
+            bg="transparent"
+            margin={5}
+            justifyContent="center"
+            alignItems="center"
+            templateColumns="repeat(6, 1fr)"
+            gap={6}
+          >
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Feeling
+            </Box>
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Comprehension
+            </Box>
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Support
+            </Box>
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Comments
+            </Box>
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Delete
+            </Box>
+            <Box
+              fontWeight="bold"
+              textAlign="center"
+              padding={1}
+              fontSize="20px"
+              w="100%"
+              h="10"
+              bg="blue.800"
+            >
+              Flag
+            </Box>
             {feedbackData.map((dataObj) => {
               return (
-                <tr key={dataObj.id}>
-                  <td>{dataObj.feeling}</td>
-                  <td>{dataObj.understanding}</td>
-                  <td>{dataObj.support}</td>
-                  <td>{dataObj.comments}</td>
-                  <td>
+                <Fragment key={dataObj.id}>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                    fontSize="1.5em"
+                  >
+                    {dataObj.feeling}
+                  </Box>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                    fontSize="1.5em"
+                  >
+                    {dataObj.understanding}
+                  </Box>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                    fontSize="1.5em"
+                  >
+                    {dataObj.support}
+                  </Box>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                    fontSize="1.5em"
+                  >
+                    {dataObj.comments}
+                  </Box>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                  >
                     <Button
                       size="sm"
                       onClick={() => handleDelete(dataObj.id)}
@@ -77,8 +183,14 @@ export default function Admin() {
                     >
                       Delete
                     </Button>
-                  </td>
-                  <td>
+                  </Box>
+                  <Box
+                    textAlign="center"
+                    textAlign="center"
+                    padding={1}
+                    w="100%"
+                    h="10"
+                  >
                     {!dataObj.flagged ? (
                       <Button
                         size="sm"
@@ -100,11 +212,64 @@ export default function Admin() {
                         Unflag
                       </Button>
                     )}
-                  </td>
-                </tr>
+                  </Box>
+                </Fragment>
               );
             })}
-          </table>
+          </Grid>
+
+          {/* <table>
+              <tr>
+                <th>Feeling</th>
+                <th>Comprehension</th>
+                <th>Support</th>
+                <th>Comments</th>
+                <th>Delete</th>
+                <th>Flag</th>
+              </tr>
+              {feedbackData.map((dataObj) => {
+                return (
+                  <tr key={dataObj.id}>
+                    <td>{dataObj.feeling}</td>
+                    <td>{dataObj.understanding}</td>
+                    <td>{dataObj.support}</td>
+                    <td>{dataObj.comments}</td>
+                    <td>
+                      <Button
+                        size="sm"
+                        onClick={() => handleDelete(dataObj.id)}
+                        variantColor="red"
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                    <td>
+                      {!dataObj.flagged ? (
+                        <Button
+                          size="sm"
+                          variantColor="red"
+                          onClick={() =>
+                            handleUpdate(dataObj.id, dataObj.flagged)
+                          }
+                        >
+                          Flag
+                        </Button>
+                      ) : (
+                        <Button
+                          size="sm"
+                          variantColor="yellow"
+                          onClick={() =>
+                            handleUpdate(dataObj.id, dataObj.flagged)
+                          }
+                        >
+                          Unflag
+                        </Button>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </table> */}
         </Box>
       </Grid>
     </div>
